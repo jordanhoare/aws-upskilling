a16e3044ee0d1ab4440b3f7464a72bd8c8c4d917
jordanhoare <jordanhoare0@gmail.com>
2022-03-16 11:16:27 +1000

    API Gateway handler

--- aws_upskilling/hit_counter/hit_counter/hit_counter_stack.py
+++ aws_upskilling/hit_counter/hit_counter/hit_counter_stack.py
@@ -3,11 +3,14 @@ from aws_cdk import aws_apigateway as apigw
 from aws_cdk import aws_lambda as _lambda
 from constructs import Construct
 
+from .hitcounter import HitCounter
+
 
 class HitCounterStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # Defines an AWS Lambda resource
         my_lambda = _lambda.Function(
             self,
             "HelloHandler",
@@ -16,8 +19,14 @@ class HitCounterStack(Stack):
             handler="hello.handler",
         )
 
+        hello_with_counter = HitCounter(
+            self,
+            "HelloHitCounter",
+            downstream=my_lambda,
+        )
+
         apigw.LambdaRestApi(
             self,
             "Endpoint",
-            handler=my_lambda,
+            handler=hello_with_counter._handler,
         )
---
+++ aws_upskilling/hit_counter/hit_counter/hitcounter.py
@@ -0,0 +1,35 @@
+from aws_cdk import aws_dynamodb as ddb
+from aws_cdk import aws_lambda as _lambda
+from constructs import Construct
+
+
+class HitCounter(Construct):
+    @property
+    def handler(self):
+        return self._handler
+
+    def __init__(
+        self, scope: Construct, id: str, downstream: _lambda.IFunction, **kwargs
+    ):
+        super().__init__(scope, id, **kwargs)
+
+        # We defined a DynamoDB table with path as the partition key (every DynamoDB table must have a single partition key).
+        table = ddb.Table(
+            self,
+            "Hits",
+            partition_key={"name": "path", "type": ddb.AttributeType.STRING},
+        )
+
+        # We defined a Lambda function which is bound to the lambda/hitcount.handler code.
+        # We wired the Lambda’s environment variables to the function_name and table_name of our resources.
+        self._handler = _lambda.Function(
+            self,
+            "HitCountHandler",
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            handler="hitcounter.handler",
+            code=_lambda.Code.from_asset("lambda"),
+            environment={
+                "DOWNSTREAM_FUNCTION_NAME": downstream.function_name,
+                "HITS_TABLE_NAME": table.table_name,
+            },
+        )
